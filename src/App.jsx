4fd6c22fdfea6ba551736cfdb436930c06dfbaab
Cláudio Santos <cljos@hotmail.com>
2024-06-08 19:54:11 -0300

    chore(css): add local css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Post author="Cláudio Santos" content="lorem teste" />
-      <Post author="Aldo Campos" content="Menino lindo" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Cláudio Santos" content="lorem teste" />
+          <Post author="Aldo Campos" content="Menino lindo" />
+        </main>
+      </div>
     </>
   );
 }
